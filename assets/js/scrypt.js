7239cfbd17bec46869b661023b62040ea721181b
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-17 15:18:32 +0100

    impostato le variabili dei prompt

--- assets/js/scrypt.js
+++ assets/js/scrypt.js
@@ -1,7 +1,19 @@
 /*
+
 1. chiedo il nome all'utente e salvo in una variabile
 2. chiedo il cognome e salvo in un'altra variabile 
 3. chiedo il colore preferito e salvo in variabile
 4. compongo la pwd e la salvo in una costante
 5. scrivo la stringa nella pagina
+
 */
+
+const name = prompt('Inserisci Nome');
+
+console.log('name',name);
+
+const surname = prompt('Inserisci Cognome');
+
+const favColour = prompt('Qual è il tuo colore preferito?')
+
+console.log(name, surname, favColour);
