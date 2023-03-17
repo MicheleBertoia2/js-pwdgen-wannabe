b1267f4d224f115db303f41f91c2b1a813f14c4c
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-17 15:12:44 +0100

    first commit

---
+++ assets/js/scrypt.js
@@ -0,0 +1,7 @@
+/*
+1. chiedo il nome all'utente e salvo in una variabile
+2. chiedo il cognome e salvo in un'altra variabile 
+3. chiedo il colore preferito e salvo in variabile
+4. compongo la pwd e la salvo in una costante
+5. scrivo la stringa nella pagina
+*/
